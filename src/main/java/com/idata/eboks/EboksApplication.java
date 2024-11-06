668ed75e90c495a9ee6a3243f280603f3535aea7
mrsluggan <ericflyger@gmail.com>
2024-11-06 09:06:00 +0100

    first comit

---
+++ src/main/java/com/idata/eboks/EboksApplication.java
@@ -0,0 +1,13 @@
+package com.idata.eboks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EboksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EboksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idata/eboks/EboksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idata.eboks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EboksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
