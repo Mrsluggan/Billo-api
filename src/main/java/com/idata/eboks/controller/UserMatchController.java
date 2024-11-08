f6f1e5b4a8f93e885432e01f50270bac166d6aac
mrsluggan <ericflyger@gmail.com>
2024-11-08 13:41:57 +0100

    Usermatch REST

---
+++ src/main/java/com/idata/eboks/controller/UserMatchController.java
@@ -0,0 +1,29 @@
+package com.idata.eboks.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.idata.eboks.Services.UserMatchService;
+import com.idata.eboks.models.UserMatch;
+
+@RestController
+@RequestMapping("/v1/tenant")
+public class UserMatchController {
+
+    @Value("${billo.api.client.tenantKey}")
+    private String tenantKey;
+
+    @Autowired
+    private UserMatchService userMatchService;
+
+
+    // Hämtar alla mottagare för tenants
+    @PostMapping("/{tenantKey}/usermatch")
+    public ResponseEntity<UserMatch> matchUsers(@PathVariable String tenantKey, @RequestBody UserMatch userMatchRequest) {
+        UserMatch response = userMatchService.matchUsers(userMatchRequest);
+        return ResponseEntity.ok(response);
+    }
+}
