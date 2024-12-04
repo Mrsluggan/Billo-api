b5c07beb5bb6702afacd06a4dcca6105c61cbeca
samsam1992 <143456029+Samback92@users.noreply.github.com>
2024-12-04 13:39:39 +0100

    Update TokenService.java

--- src/main/java/com/idata/eboks/Services/TokenService.java
+++ src/main/java/com/idata/eboks/Services/TokenService.java
@@ -1,33 +1,49 @@
 package com.idata.eboks.Services;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @Service
 public class TokenService extends BaseService {
-    @Value("${billo.api.client.id}")
-    private String clientId;
 
-    @Value("${billo.api.client.secret}")
+    private String clientId;
     private String clientSecret;
 
-    @Autowired
     private WebClient.Builder webClientBuilder;
+
+    @Autowired
+    public TokenService(WebClient.Builder webClientBuilder) {
+        this.webClientBuilder = webClientBuilder;
+        loadDotenv();
+    }
+
+    private void loadDotenv() {
+        Dotenv dotenv = Dotenv.load();
+        this.clientId = dotenv.get("CLIENT_ID");
+        this.clientSecret = dotenv.get("CLIENT_SECRET");
+        System.out.println("clientId: " + clientId + " clientSecret: " + clientSecret);
+    }
 
     public String getAccessToken() {
         try {
-
             WebClient webClient = webClientBuilder.build();
+            String encodedClientId = URLEncoder.encode(clientId, StandardCharsets.UTF_8);
+            String encodedClientSecret = URLEncoder.encode(clientSecret, StandardCharsets.UTF_8);
             String token = webClient.post()
                     .uri("https://sandbox-identity.billo.life/connect/token")
                     .header("Content-Type", "application/x-www-form-urlencoded")
-                    .bodyValue(String.format("grant_type=client_credentials&client_id=%s&client_secret=%s", clientId,
-                            clientSecret))
+                    .bodyValue(String.format("grant_type=client_credentials&client_id=%s&client_secret=%s", encodedClientId,
+                    encodedClientSecret))
                     .retrieve()
                     .bodyToMono(String.class)
                     .block();
+                    System.out.println("token: " + token);
 
             String[] parts = token.split("\"");
             return parts[3];
