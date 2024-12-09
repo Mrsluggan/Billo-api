f858a8a280e2a9ce1db727a05a1cef7bea0a61d0
samsam1992 <143456029+Samback92@users.noreply.github.com>
2024-12-09 10:11:16 +0100

    added logger

--- src/main/java/com/idata/eboks/Services/BaseService.java
+++ src/main/java/com/idata/eboks/Services/BaseService.java
@@ -1,11 +1,14 @@
 package com.idata.eboks.Services;
 
-import com.idata.eboks.configs.auth.BilloApiConfig;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.*;
+
+import com.idata.eboks.configs.auth.BilloApiConfig;
 
 @Service
 public class BaseService {
@@ -13,9 +16,13 @@ public class BaseService {
     @Autowired
     private BilloApiConfig billoApiConfig;
 
+    @Autowired 
+    protected RestTemplate billoApiRestTemplateBean;
+
     private final String BASE_URL = "https://sandbox-sender-api.billo.life/v1/tenant/";
 
     private String url = "https://sandbox-sender-api.billo.life/v1/tenant";
+
     protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 
@@ -29,5 +36,32 @@ public class BaseService {
 
     public String getUrl() {
         return url;
+    }
+
+    public <T> T sendRequest(
+        String url,
+        HttpMethod httpMethod,
+        Object requestBody,
+        ParameterizedTypeReference<T> responseType
+    ){
+        try{
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            HttpEntity<Object>entity = new HttpEntity<>(requestBody, headers);
+
+        ResponseEntity<T> response = billoApiRestTemplateBean.exchange(url, httpMethod, entity, responseType);
+
+            logger.info("Response Status: {}", response.getStatusCode());
+            logger.info("Response Headers: {}", response.getHeaders());
+            logger.info("Response Body: {}", response.getBody());
+
+            return response.getBody();
+        } catch (HttpClientErrorException e) {
+            logger.error("Error response: {}", e.getResponseBodyAsString(), e);
+            throw e;
+        } catch (Exception e) {
+            logger.error("Error: {}", e.getMessage(), e);
+            throw new RuntimeException("Error processing request", e);
+        }
     }
 }
--- src/main/java/com/idata/eboks/Services/IconService.java
+++ src/main/java/com/idata/eboks/Services/IconService.java
@@ -1,42 +1,21 @@
 package com.idata.eboks.Services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
-
 import com.idata.eboks.models.Icon;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+
 
 @Service
 public class IconService extends BaseService {
     
-    @Autowired
-    private RestTemplate billoApiRestTemplateBean;
-    
     public Icon provideIcon(String tenantKey, Icon icon) {
-        try {
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            HttpEntity<Icon> entity = new HttpEntity<>(icon, headers);
-    
-            return billoApiRestTemplateBean.exchange(
-                createSlug(tenantKey, "/icon"),
-                HttpMethod.POST,
-                entity,
-                new ParameterizedTypeReference<Icon>() {
-                }).getBody();
-
-        } catch (HttpClientErrorException e) {
-            System.out.println("Error response: " + e.getResponseBodyAsString());
-            throw e;
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-            throw new RuntimeException("Error processing icon", e);
+        logger.info("Providing icon for tenant: {}", tenantKey);
+        if (icon == null) {
+            logger.warn("Icon is null");
+            return null;
         }
+        String url = createSlug(tenantKey, "/icon");
+        return sendRequest(url, HttpMethod.POST, icon, new ParameterizedTypeReference<Icon>() {});
     }
 }
